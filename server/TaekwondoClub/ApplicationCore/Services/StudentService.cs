3962cbe671a1f3a5c30fc9b9d34d89492367b69c
TheDayIsMyEnemy <tthedayismyenemy@gmail.com>
2022-04-03 00:06:56 +0300

    Add some validations for student

--- server/TaekwondoClub/ApplicationCore/Services/StudentService.cs
+++ server/TaekwondoClub/ApplicationCore/Services/StudentService.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationCore.Interfaces;
 using ApplicationCore.Models;
+using System.Text.RegularExpressions;
 
 namespace ApplicationCore.Services
 {
@@ -20,7 +21,9 @@ namespace ApplicationCore.Services
             string? phoneNumber,
             DateTime[]? membershipPeriod)
         {
-            // add validations
+            if (!ValidateStudent(firstName, lastName, gender, birthDate, phoneNumber, membershipPeriod))
+                return false;
+
             var student = new Student(firstName, lastName, gender, birthDate, phoneNumber);
 
             if (membershipPeriod != null)
@@ -41,6 +44,31 @@ namespace ApplicationCore.Services
             {
                 return false;
             }
+
+            return true;
+        }
+
+        private bool ValidateStudent(
+            string firstName,
+            string lastName,
+            string gender,
+            DateTime? birthDate,
+            string? phoneNumber,
+            DateTime[]? membershipPeriod)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+                return false;
+            if (string.IsNullOrWhiteSpace(lastName))
+                return false;
+            if (!gender.Equals("Male") && !gender.Equals("Female"))
+                return false;
+            if (birthDate.HasValue && birthDate.Value >= DateTime.Now)
+                return false;
+            if (phoneNumber != null && !Regex.IsMatch(phoneNumber, @"^0\d{9}$|^359\d{9}$"))
+                return false;
+            if (membershipPeriod != null &&
+                (membershipPeriod.Length != 2 || membershipPeriod[0] < DateTime.Now || membershipPeriod[0] >= membershipPeriod[1]))
+                return false;
 
             return true;
         }
